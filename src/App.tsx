a2811e898acf9c49ee8f3d13c2a4766c04ef3028
Fabian <fabiane@disroot.org>
2019-07-22 10:16:24 +0200

    Added Tile

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Tile } from './components/Tile/Tile';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-   
+      <Tile click={() => {}} id={1}  light={1} />
     </div>
   );
 }
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,5 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, FC } from 'react';
+import styles from './Tile.module.sass'
 
-export const Light = () => {
-    const [state, setstate] = useState(initialState)
+enum Light {
+	on,
+	off
+}
+
+interface IProps {
+	id: number;
+	click: any;
+	light: Light;
+}
+
+export const Tile:React.FC<IProps> = (props: IProps) => {
+	const [lights, setLights] = useState<Light>(Light.off)
+	const lightsColor = Light.on ? styles.lightOn : styles.lightOff; 
+	const handleClick = () => {
+		setLights(lights === Light.off ? Light.on : Light.off);
+		props.click({ id: props.id, light: lights });
+	}
+
+	return (
+		<>
+			<button className={`${styles.tile} ${lightsColor}`} onClick={handleClick}></button>
+		</>
+	)
 }
